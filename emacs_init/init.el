1e9d727c6686e5920567796fcca80cdec790c531
jayps95 <jamesacheung@gmail.com>
2020-06-06 01:31:51 +0800

    To be placed in ~/.emacs.d , init file contains load jayps_emacs_base.el

---
+++ emacs_init/init.el
@@ -0,0 +1,21 @@
+;;;;;;; init file ;;;;;
+
+;;; Refrence folder with relevant emacs files ;;;
+(load "~/Documents/git/emacs_init/jayps_emacs_base.el")
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elpy-shell-echo-input nil)
+ '(package-selected-packages
+   (quote
+    (spacemacs-theme flycheck iedit which-key use-package pyenv-mode material-theme magit-popup magit ghub exec-path-from-shell elpy better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
